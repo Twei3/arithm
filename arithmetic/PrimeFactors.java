f4bce7ac25298c6db836c51d484e9e96d66ab724
Twei3 <tw1156292325@163.com>
2016-11-30 18:43:35 +0800

    Create PrimeFactors.java

---
+++ arithmetic/PrimeFactors.java
@@ -0,0 +1,21 @@
+package com.efront.junit.exercise.part01.main;
+/**
+ * 将一个正整数分解质因数
+ * @author tangwei
+ *
+ */
+public class PrimeFactors {
+
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		StringBuffer sbf = new StringBuffer();
+		for(int i=2; i <= n;i++){
+			while(n%i == 0){
+				n = n/i;
+				sbf.append(i+" ");
+			}
+		}
+		System.out.print(sbf.toString().trim().replace(" ", "*"));
+	}
+
+}
